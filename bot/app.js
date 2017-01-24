02ed322a1de91e123d99b41fbc9794c39744e0bf
Leo Bekhuis <lbekhuis@gmail.com>
2017-01-24 15:56:20 +0100

    add first bot files

---
+++ bot/app.js
@@ -0,0 +1,8 @@
+//=========================================================
+// Seahorse Alvis conversation channel https://alvis.io
+// Azure dev.botframework.com nodejs version for multiple channels
+// Authenticate through uport pasport
+// Hyperledger Fabric Client connectivity with the IBM Bluemix blockchain
+// Goal is storing from chat > hfc > uport > bigchaindb > chat
+// Boilerrplate chatbot for Team Seahorse #dbh17 Groningen
+//=========================================================
